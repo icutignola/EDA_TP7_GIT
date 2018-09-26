d31ece0e42d1590d5549364d2c159a9a05249c50
Ignacio Cutignola <cutignolai@gmail.com>
2018-09-26 11:24:39 -0300

    Creo proyecto

---
+++ EDA_TP7/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+//**********  Define Puertos  ***********
+#define PORT_P0	0
+#define PORT_P1	1
+#define PORT_P2	2
+#define PORT_P3	3
+#define PORT_P4	4
+#define PORT_P5	5
+#define PORT_P6	6
+#define PORT_P7	7
+//***************************************
+
+//*************  Define E  **************
+#define LCD_E		(1<<PORT_P0)
+#define LCD_E_ON	(LCD_E)
+#define LCD_E_OFF	(LCD_E ^ LCD_E)
+//***************************************
+
+//*************  Define RS  *************
+#define LCD_RS		(1<<PORT_P1)
+#define LCD_RS_ON	(LCD_RS)
+#define LCD_RS_OFF	(LCD_RS ^ LCD_RS)
+//***************************************
+
+//*************	Instrucciones del LCD	***********
+#define LCD_CLEAR					0x01
+#define LCD_RETURN_HOME				0x02
+#define LCD_ENTRY_MODE_SET			0x03
+#define LCD_DISPLAY_ON_OF_CONTROL	0x04
+#define LCD_DISPLAY_SHIFT			0x05
+#define LCD_FUNCTION_SET			0x06
+#define LCD_SET_CGRAM_ADDRESS		0x07
+#define LCD_DDRAM_ADRESS			0x08
+#define LCD_READ_BUSY_FLAG_ADDRESS	0x09
+//*************************************************
+
+//************* Funciones de bajo nivel **********
+
+//Inicializa el FTDI y el LCD
+FT_HANDLE * deviceHandler lcdInit(int iDevice);
+//Escribe un byte al IR
+void lcdWriteIR(FT_HANDLE * deviceHandler, BYTE valor);
+//Escribe un byte al DR 
+void lcdWriteDR(FT_HANDLE * deviceHandler, BYTE valor);
