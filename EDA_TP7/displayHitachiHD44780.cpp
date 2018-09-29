b1e6fb9bcc00fac3ef3b68272e3c001314651dd2
Thomas Neuhaus <thomneuhaus98@live.com.ar>
2018-09-29 15:38:07 -0300

    we0of

--- EDA_TP7/displayHitachiHD44780.cpp
+++ EDA_TP7/displayHitachiHD44780.cpp
@@ -1,12 +1,21 @@
 #include "displayHitachiHD44780.h"
-
+#include <iostream>
+#include <cstdio>
 
 
 displayHitachiHD44780::displayHitachiHD44780()
 {
+	lcdPointer = init_ftdi_lcd(LCD_NUMBER);
+		
 }
 
 
 displayHitachiHD44780::~displayHitachiHD44780()
 {
+}
+
+bool 
+displayHitachiHD44780::lcdInitOk()
+{
+	return(lcdPointer == NULL);
 }
--- EDA_TP7/displayHitachiHD44780.h
+++ EDA_TP7/displayHitachiHD44780.h
@@ -6,5 +6,22 @@ class displayHitachiHD44780 :
 public:
 	displayHitachiHD44780();
 	~displayHitachiHD44780();
+	virtual bool lcdInitOk();
+	virtual FT_STATUS lcdGetError();
+	virtual bool lcdClear();
+	virtual bool lcdClearToEOL();
+	virtual basicLCD& operator<<(const unsigned char c);
+	virtual basicLCD& operator<<(const unsigned char * c);
+	virtual bool lcdMoveCursorUp();
+	virtual bool lcdMoveCursorDown();
+	virtual bool lcdMoveCursorRight();
+	virtual bool lcdMoveCursorLeft();
+	virtual bool lcdSetCursorPosition(const cursorPosition pos);
+	virtual cursorPosition lcdGetCursorPosition();
+	
+
+private:
+	FT_HANDLE * lcdPointer;
+
 };
 
